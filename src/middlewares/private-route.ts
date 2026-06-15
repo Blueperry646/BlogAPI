28fbdffb5884f41d623adb61363617c2dff8820e
Yan <yanmpenteado@gmail.com>
2026-06-15 08:37:26 -0300

    fazendo login

---
+++ src/middlewares/private-route.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import { verifyRquest } from "../services/auth.service";
+import { ExtendedRequest } from "../types/extended-request";
+
+export const privateRoute = (
+    req: ExtendedRequest,
+    res: Response,
+    next: NextFunction
+) => { 
+    const user = await verifyRquest(req);
+    if (!user) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    req.user = user;
+    next();
+}
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,6 +3,6 @@ import * as authController from '../controllers/auth.controller';
 
 export const authRoutes = Router();
 
-authRoutes.post('/sigin', authController.signIn);
+authRoutes.post('/signin', authController.signIn);
 authRoutes.post('/signup', authController.signUp);
 authRoutes.post('/validate', authController.validate);
